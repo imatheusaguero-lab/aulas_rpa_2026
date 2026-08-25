bde3618d7413d6235ba8a09cf15e0b6a47acdff1
Matheus Santos Aguero <imatheusaguero@gmail.com>
2026-08-25 19:26:05 -0300

    push

---
+++ AULA01/bot_initializer.py
@@ -0,0 +1,20 @@
+BOT_NAME= "RPA_FINANCEIRO_01"
+MAX_RETRIES = 3
+EXECUTION_TIMEOUT = 180.5 
+IS_PRODUCTION = False
+
+print("Nome do robô: ", BOT_NAME)
+print("Tipo: ", type(BOT_NAME))
+print()
+
+print("Tentativas: ", MAX_RETRIES)
+print("Tipo: ", type(MAX_RETRIES))
+print()
+
+print("Tempo limite", EXECUTION_TIMEOUT)
+print("Tipo: ", type(EXECUTION_TIMEOUT))
+print()
+
+print("Ambiente de Produção: ", IS_PRODUCTION)
+print("Tipo: ", type(IS_PRODUCTION))
+print()
